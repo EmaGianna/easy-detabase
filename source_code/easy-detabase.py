2c8fd904e25194504e098ea224c323c399f3c372
Emanuel Giannattasio <emanuel.giannattasio@gmail.com>
2022-11-04 16:10:46 -0300

    change_ bulk_insert_to_deta
    change on bulk_insert_to_deta function

--- source_code/easy-detabase.py
+++ source_code/easy-detabase.py
@@ -16,26 +16,49 @@ def deta_table_to_dataframe(deta_dict):
     return df.reset_index(drop=True)
 
 
-def bulk_insert_to_deta(csv_path_file, separator, deta_base):
+def bulk_insert_to_deta(df_aux, deta_base, chunksize=25):
     """
-    It takes a csv file, reads it into a pandas dataframe, converts the dataframe to a dictionary, and
-    then inserts each row of the dataframe into a deta table
+    It takes a dataframe, a deta base, and a chunk size, and inserts the dataframe into the deta base in
+    chunks of the specified size
     
-    :param csv_path_file: the path to the csv file you want to insert into deta
-    :param separator: the separator used in the csv file
-    :param deta_base: the name of the deta base you want to insert into
+    :param df_aux: dataframe to insert
+    :param deta_base: the name of the table you want to insert data into
+    :param chunksize: The number of records to insert at a time, defaults to 25 (optional)
     """
-    df = pd.read_csv(csv_path_file, sep = separator)
-    dict = df.to_dict()
-    columns = df.columns
+
+    index_upper = 0
+    counter_ok = 0
+    counter_nok = 0
     dict_to_insert ={}
-    counter = 0
-    for index in range(len(df)):
-        for key in columns:
-            dict_to_insert[key] = dict[key][index] 
-        deta_base.insert(dict_to_insert)
-        counter = counter +1 
-    print(f'{counter} records was inserted')
+    for i in range(0,len(df_aux),chunksize):
+        index_lower = i
+        index_upper = index_upper + chunksize
+        print(df_aux.iloc[index_lower:index_upper])
+        df = df_aux.iloc[index_lower:index_upper]
+        list_to_insert =[]
+        df = df.reset_index(drop=True)
+        dict = df.to_dict()
+        columns = df.columns
+        for index in range(df.shape[0]):
+            for key in columns:
+                dict_to_insert[key] = dict[key][index]
+                dict_copy = dict_to_insert.copy()
+            list_to_insert.append(dict_copy)
+        try:
+            deta_base.put_many(list_to_insert)
+            counter_ok = counter_ok + len(list_to_insert)
+        except Exception as e:
+            counter_nok = counter_nok + len(list_to_insert)
+            if (len(list_to_insert) <= 25):
+                print(f' {e}\n Something was wrong with some record.\n Put value 1 to insert record and check the issue')
+                print(' Suggestion: is good idea create output.log file if you try insert a lot of records;')
+                print(' in that way you can find the error message on it and check.')
+            else:
+                print(e)
+            continue
+    print(f'Chunk step {chunksize}')
+    print(f'{counter_ok} records was inserted')
+    print(f'{counter_nok} records was no inserted')
 
 
 def truncate_deta_table(deta_base):
